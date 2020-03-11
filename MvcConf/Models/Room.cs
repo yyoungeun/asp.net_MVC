31fd03dd478d6cf1ae6761d176e225373a2140e4
yyoungeun <1150amy@naver.com>
2020-03-11 14:14:54 +0900

    MVC commit

---
+++ MvcConf/Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcConf.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Speaker> Speakers { get; set; }
+    }
+    public class Speaker
+    {
+        public int SpeakerId { get; set; }
+        public string Name { get; set; }
+        public int RoomId { get; set; }
+
+        public virtual Room Room { get; set; }
+    }
+}
